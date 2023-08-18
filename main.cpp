6b71ecf96e0b6438653f43a00b971c09c647280c
Keith Kyzivat <keith.kyzivat@qt.io>
2023-08-18 17:32:17 -0400

    Use qDebug not Q_ASSERT, add QHoverEvent checking
    * Remove Q_ASSERT_X usage, so we can see all of the failure cases.
    * Add QHoverEvent when compiling against Qt >= 6.2, which made
      QHoverEvent derive from QSinglePointEvent, and thus be affected by
      this bug.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <QApplication>
 #include <QMouseEvent>
-#include <QHoverEvent>
+#include <QDebug>
 #include <QtGlobal>
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 2, 0)
+#include <QHoverEvent>
+#endif
 
 QString toString(QPointF point)
 {
     return QString("(%1, %2)").arg(point.x()).arg(point.y());
+}
+
+void validatePointEvent(const QPointF& beforePoint, const QPointF& afterPoint, const QString& where, const QString& formatErrMsg)
+{
+    if (beforePoint != afterPoint) {
+        qDebug().noquote().nospace() << "Failure " << where << ": " << formatErrMsg.arg(toString(beforePoint), toString(afterPoint));
+    }
 }
 
 int main(int argc, char *argv[])
@@ -15,26 +26,28 @@ int main(int argc, char *argv[])
     QPointF mePoint1(1,1);
     QPointF mePoint1Glob(10,10);
     QMouseEvent *me1 = new QMouseEvent(QEvent::MouseMove, mePoint1, mePoint1Glob, Qt::NoButton, Qt::NoButton, Qt::NoModifier);
-    Q_ASSERT_X(me1->scenePosition() == mePoint1, "After first event created", QString("Unexpected scenePosition in first point event. Expected %1, Actual %2").arg(toString(mePoint1), toString(me1->scenePosition())).toLocal8Bit());
-    Q_ASSERT_X(me1->globalPosition() == mePoint1Glob, "After first event created", QString("Unexpected globalPosition in first point event. Expected %1, Actual %2").arg(toString(mePoint1Glob), toString(me1->globalPosition())).toLocal8Bit());
+    validatePointEvent(mePoint1, me1->scenePosition(), "After first event created", "Unexpected scenePosition in first point event. Expected %1, Actual %2");
+    validatePointEvent(mePoint1Glob, me1->globalPosition(), "After first event created", "Unexpected globalPosition in first point event. Expected %1, Actual %2");
 
     QPointF mePoint2(2, 2);
     QPointF mePoint2Glob(20, 20);
-    QMouseEvent *me2 = new QMouseEvent(QEvent::HoverMove, mePoint2, mePoint2Glob, mePoint2, Qt::NoButton, Qt::NoButton, Qt::NoModifier);
-    Q_ASSERT_X(me2->scenePosition() == mePoint2, "After second event created", QString("Unexpected scenePosition in second point event. Expected %1, Actual %2").arg(toString(mePoint2), toString(me2->scenePosition())).toLocal8Bit());
-    Q_ASSERT_X(me2->globalPosition() == mePoint2Glob, "After second event created", QString("Unexpected globalPosition in second point event. Expected %1, Actual %2").arg(toString(mePoint2Glob), toString(me2->globalPosition())).toLocal8Bit());
+    QMouseEvent *me2 = new QMouseEvent(QEvent::MouseMove, mePoint2, mePoint2Glob, Qt::NoButton, Qt::NoButton, Qt::NoModifier);
+    validatePointEvent(mePoint2, me2->scenePosition(), "After second event created", "Unexpected scenePosition in second point event. Expected %1, Actual %2");
+    validatePointEvent(mePoint2Glob, me2->globalPosition(), "After second event created", "Unexpected globalPosition in second point event. Expected %1, Actual %2");
 
-    Q_ASSERT_X(me1->scenePosition() == mePoint1, "After second event created", QString("Unexpected scenePosition in first point event. Expected %1, Actual %2").arg(toString(mePoint1), toString(me1->scenePosition())).toLocal8Bit());
-    Q_ASSERT_X(me1->globalPosition() == mePoint1Glob, "After second event created", QString("Unexpected globalPosition in first point event. Expected %1, Actual %2").arg(toString(mePoint1Glob), toString(me1->globalPosition())).toLocal8Bit());
+    validatePointEvent(mePoint1, me1->scenePosition(), "After second event created", "Unexpected scenePosition in first point event. Expected %1, Actual %2");
+    validatePointEvent(mePoint1Glob, me1->globalPosition(), "After second event created", "Unexpected globalPosition in first point event. Expected %1, Actual %2");
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 2, 0)
     QPointF hePoint(3, 3);
     QPointF hePointGlob(30, 30);
     QHoverEvent *he = new QHoverEvent(QEvent::HoverMove, hePoint, hePointGlob, hePoint, Qt::NoModifier);
-    Q_ASSERT_X(he->scenePosition() == hePoint, "After third (hover) event created", QString("Unexpected scenePosition in third (hover) point event. Expected %1, Actual %2").arg(toString(hePoint), toString(he->scenePosition())).toLocal8Bit());
-    Q_ASSERT_X(he->globalPosition() == hePointGlob, "After third (hover) event created", QString("Unexpected globalPosition in third (hover) point event. Expected %1, Actual %2").arg(toString(hePointGlob), toString(he->globalPosition())).toLocal8Bit());
+    validatePointEvent(hePoint, he->scenePosition(), "After third (hover) event created", "Unexpected scenePosition in third (hover) point event. Expected %1, Actual %2");
+    validatePointEvent(hePointGlob, he->globalPosition(), "After third (hover) event created", "Unexpected globalPosition in third (hover) point event. Expected %1, Actual %2");
 
-    Q_ASSERT_X(me1->scenePosition() == mePoint1, "After third (hover) event created", QString("Unexpected scenePosition in first point event. Expected %1, Actual %2").arg(toString(mePoint1), toString(me1->scenePosition())).toLocal8Bit());
-    Q_ASSERT_X(me1->globalPosition() == mePoint1Glob, "After third (hover) event created", QString("Unexpected globalPosition in first point event. Expected %1, Actual %2").arg(toString(mePoint1Glob), toString(me1->globalPosition())).toLocal8Bit());
+    validatePointEvent(mePoint1, me1->scenePosition(), "After third (hover) event created", "Unexpected scenePosition in first point event. Expected %1, Actual %2");
+    validatePointEvent(mePoint1Glob, me1->globalPosition(), "After third (hover) event created", "Unexpected globalPosition in first point event. Expected %1, Actual %2");
+#endif
 
     return 0;
 }
